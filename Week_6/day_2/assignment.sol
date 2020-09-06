d5816b4b564d03013259e89533f2bdf79cb8e7ab
creative-content <68776276+creative-content@users.noreply.github.com>
2020-09-06 10:16:14 +0100

    Update assignment.sol
    People update assignment

--- Week_6/day_2/assignment.sol
+++ Week_6/day_2/assignment.sol
@@ -1,124 +1,39 @@
 pragma solidity 0.5.12;
 
-contract HelloWorld{
+contract MyContract {
+    uint[] private list;
 
-    struct Person {
-      uint id;
-      string name;
-      uint age;
-      uint height;
-      bool senior;
+    struct Item {
+        bool valid;
+        uint index;
+        uint age;
+        uint height;
+
+        string name;
     }
 
-    address public owner;
+    event personDataEntered(string name, uint age, uint height);
+    mapping(uint => Item) public data;
 
-    constructor() public{
-        owner = msg.sender;
+
+
+    /* Update or insert an item */
+    function person(uint ID, uint age, uint height, string calldata name) external {
+        Item storage item = data[ID];
+        if (!item.valid) {
+            item.valid = true;
+            item.index = list.length;
+            list.push(ID);
+        }
+        item.age = age;
+        item.height = height;
+        item.name = name;
+
+         assert(keccak256(abi.encodePacked(name,age,height))==keccak256(abi.encodePacked(name,age,height)));
+         emit personDataEntered(name,age,height);
     }
 
-    mapping (address => Person) private people;
-    address[] private creators;
-
-    function createPerson(string memory name, uint age, uint height) public {
-      require(age < 150, "Age needs to be below 150");
-        //This creates a person
-        Person memory newPerson;
-        newPerson.name = name;
-        newPerson.age = age;
-        newPerson.height = height;
-
-        if(age >= 65){
-           newPerson.senior = true;
-       }
-       else{
-           newPerson.senior = false;
-       }
-
-        insertPerson(newPerson);
-        creators.push(msg.sender);
-
-        assert(
-            keccak256(
-                abi.encodePacked(
-                    people[msg.sender].name,
-                    people[msg.sender].age,
-                    people[msg.sender].height,
-                    people[msg.sender].senior
-                )
-            )
-            ==
-            keccak256(
-                abi.encodePacked(
-                    newPerson.name,
-                    newPerson.age,
-                    newPerson.height,
-                    newPerson.senior
-                )
-            )
-        );
-    }
-
-    function insertPerson(Person memory newPerson) private {
-        address creator = msg.sender;
-        people[creator] = newPerson;
-    }
-    function getPerson() public view returns(string memory name, uint age, uint height, bool senior){
-        address creator = msg.sender;
-        return (people[creator].name, people[creator].age, people[creator].height, people[creator].senior);
-    }
-    function updatePerson(string memory name, uint age, uint height) public {
-      require(age < 150, "Age needs to be below 150");
-        //This updates a person
-        Person memory changePerson;
-        changePerson.name = name;
-        changePerson.age = age;
-        changePerson.height = height;
-
-        if(age >= 65){
-           changePerson.senior = true;
-       }
-       else{
-           changePerson.senior = false;
-       }
-
-        insertChangedPerson(changePerson);
-        creators.push(msg.sender);
-
-        assert(
-            keccak256(
-                abi.encodePacked(
-                    changePerson.name,
-                    changePerson.age,
-                    changePerson.height,
-                    changePerson.senior
-                )
-            )
-            !=
-            keccak256(
-                 abi.encodePacked(
-                    newPersonPerson.name,
-                    newPerson.age,
-                    newPerson.height,
-                    newPerson.senior
-                )
-            )
-        );
-    }
-
-    function insertChangedPerson(Person memory changePerson) private {
-        address creator = msg.sender;
-        people[creator] = changePerson;
-    }
-    function deletePerson(address creator) public {
-       require(msg.sender == owner, "Caller needs to be owner");
-       delete people[creator];
-       assert(people[creator].age == 0);
-   }
-
-   function getCreator(uint index) public view returns(address){
-       require(msg.sender == owner, "Caller needs to be owner");
-       return creators[index];
-   }
-
-
-}
+    //function count() external view returns (uint index) {
+        //return list.length;
+    //}
+  }
